76a63bc5b66514e7399d62672cf170cf4fc12c22
MiikkaSa <miikka148@gmail.com>
2024-11-09 18:10:41 +0200

    sivujen parantelua

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { AuthProvider } from "../context/AuthContext";
-
+import Navbar from "./components/Navbar";
 // src/app/layout.tsx or _app.js
 // src/app/layout.tsx
 
